a820086e450097133b77148944e178466b206333
arata-1972 <arata1972.info@gmail.com>
2017-09-03 18:30:26 +0900

    Update main.js

--- cloud/main.js
+++ cloud/main.js
@@ -3,6 +3,56 @@ Parse.Cloud.define('currentTime', function(req, res) {
 });
 
 
+Parse.Cloud.define('storyJapan', function(req, res) {
+  var StoryJapan = Parse.Object.extend("StoryJapan");
+    
+    
+    var query = new Parse.Query(StoryJapan);
+    query.equalTo("level",parseInt(req.params.level));
+  query.limit(1000);
+//   Array.prototype.randomElement = function(min,max){
+  
+//   return Math.floor(Math.random()*(max - min)) + min;
+  
+  
+//   }
+  
+  query.find({
+    
+    success: function(results){
+      
+//      var final10 =[];
+      
+//       for(var i =0; i < 10; i++){
+//         var random = results.randomElement(0,results.length);
+        
+//         if (final10.indexOf(random) == -1){
+//           final10.push(random);
+//         }else{
+          
+//           i--;
+//         }
+//       }
+      
+      var quiz = [];
+      for(var j =0; j < results.length; j++){
+      
+        var index = results[j];
+        quiz.push(results[index]);
+      }
+      //res.success(quiz);
+      res.success(JSON.stringify(quiz));
+    },
+    error: function(e){
+    res.error(e);
+    
+    
+    
+    }
+  });
+});
+
+
 
 Parse.Cloud.define('randomQuiz', function(req, res) {
  
